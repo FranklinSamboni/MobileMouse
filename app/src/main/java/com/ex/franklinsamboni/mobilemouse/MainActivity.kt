0b749a62b0ad2c87b9f79c35e6db644638cff978
Franklin Samboní Castillo <franklin.samboni@PRAGMA.COM.CO>
2019-02-21 18:07:45 -0500

    blue

--- app/src/main/java/com/ex/franklinsamboni/mobilemouse/MainActivity.kt
+++ app/src/main/java/com/ex/franklinsamboni/mobilemouse/MainActivity.kt
@@ -3,6 +3,8 @@ package com.ex.franklinsamboni.mobilemouse
 import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothServerSocket
+import android.bluetooth.BluetoothSocket
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -25,6 +27,9 @@ import java.util.*
 import android.os.AsyncTask
 import android.os.AsyncTask.execute
 import org.json.JSONObject
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
 
 
 const val REQUEST_ENABLE_BT = 100
@@ -45,7 +50,9 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     lateinit var mWindowManager: WindowManager
     lateinit var mDisplay: Display
 
-    lateinit var echoClient : EchoClient
+    lateinit var echoClient: EchoClient
+
+    private var acceptConnection: AcceptThread? = null
 
     var accX: Float = 0.0f
     var accY: Float = 0.0f
@@ -54,6 +61,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     var mVelX: Float = 0.0f
     var mVelY: Float = 0.0f
     var lastTime: Long = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -74,11 +82,10 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
         echoClient = EchoClient()
 
-        /*mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-        if(mBluetoothAdapter == null){
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if (mBluetoothAdapter == null) {
             print("El dispositivo no es compatible para usar Bluetooth")
-        }
-        else{
+        } else {
             /*if(!mBluetoothAdapter.isEnabled){
                 var enableBt : Intent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
                 startActivityForResult(enableBt,REQUEST_ENABLE_BT)
@@ -87,7 +94,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             }*/
             val discoverableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE)
             discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300)
-            startActivityForResult(discoverableIntent,REQUEST_VISIBILITY_BT)
+            startActivityForResult(discoverableIntent, REQUEST_VISIBILITY_BT)
         }
 
         //val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
@@ -99,7 +106,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     override fun onResume() {
         super.onResume()
-        mSensorManager.registerListener(this, mSensorAcc, SensorManager.SENSOR_DELAY_NORMAL)
+        mSensorManager.registerListener(this, mSensorAcc, SensorManager.SENSOR_DELAY_GAME)
         //mSensorManager.registerListener(this, mSensorGyr, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
@@ -151,7 +158,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
                 mPosX = mVelX * dT + accX * dT * dT / 2
                 mPosY = mVelY * dT + accY * dT * dT / 2
 
-                if(mPosX > 1.0 || mPosY > 1.0){
+                if (mPosX > 1.0 || mPosY > 1.0) {
                     print(mPosX)
                 }
                 //mVelX += accX * dT
@@ -163,9 +170,15 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             lastTime = date.timeInMillis
 
             val json = JSONObject()
-            json.put( "accX",accX)
-            json.put("accY",accY)
-            SendMessageTask().execute(json.toString())
+            json.put("accX", accX)
+            json.put("accY", accY)
+
+            if(acceptConnection != null){
+                val msg  = "$json/"
+                acceptConnection!!.getConnectedThread()!!.write(msg.toByteArray())
+            }
+
+            //SendMessageTask().execute()
         }
     }
 
@@ -175,6 +188,10 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         if (requestCode == REQUEST_ENABLE_BT) {
             searchDevices()
         } else if (requestCode == REQUEST_VISIBILITY_BT) {
+            if(acceptConnection == null){
+                acceptConnection = AcceptThread(mBluetoothAdapter)
+                acceptConnection!!.run()
+            }
             print(resultCode)
         }
     }
@@ -204,11 +221,129 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         }
     }
 
-    private inner class SendMessageTask : AsyncTask<String, Void, Void>() {
+    inner class SendMessageTask : AsyncTask<String, Void, Void>() {
         override fun doInBackground(vararg jsonA: String): Void? {
 
             echoClient.sendEcho(jsonA[0])
             return null
         }
     }
+
+
+    class AcceptThread(mBluetoothAdapter: BluetoothAdapter) : Thread() {
+        private val mmServerSocket: BluetoothServerSocket?
+        private var connectedThread: ConnectedThread? = null
+
+        init {
+            // Use a temporary object that is later assigned to mmServerSocket,
+            // because mmServerSocket is final
+            var tmp: BluetoothServerSocket? = null
+            try {
+                // MY_UUID is the app's UUID string, also used by the client code
+                tmp = mBluetoothAdapter.listenUsingRfcommWithServiceRecord("J2", UUID.fromString("6d64ed24-16de-4439-8e19-9b4b5dd96737"))
+            } catch (e: IOException) {
+            }
+
+            mmServerSocket = tmp
+        }
+
+        override fun run() {
+            var socket: BluetoothSocket? = null
+            // Keep listening until exception occurs or a socket is returned
+            while (true) {
+                try {
+                    socket = mmServerSocket!!.accept()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                    break
+                }
+
+                // If a connection was accepted
+                if (socket != null) {
+                    // Do work to manage the connection (in a separate thread)
+                    manageConnectedSocket(socket)
+                    mmServerSocket.close()
+                    break
+                }
+            }
+        }
+
+        private fun manageConnectedSocket(socket: BluetoothSocket) {
+            connectedThread = ConnectedThread(socket)
+        }
+
+        fun getConnectedThread(): ConnectedThread? {
+            return this.connectedThread
+        }
+
+        /** Will cancel the listening socket, and cause the thread to finish  */
+        fun cancel() {
+            try {
+                mmServerSocket!!.close()
+            } catch (e: IOException) {
+            }
+
+        }
+    }
+
+
+    class ConnectedThread(private val mmSocket: BluetoothSocket) : Thread() {
+        private val mmInStream: InputStream?
+        private val mmOutStream: OutputStream?
+
+        init {
+            var tmpIn: InputStream? = null
+            var tmpOut: OutputStream? = null
+
+            // Get the input and output streams, using temp objects because
+            // member streams are final
+            try {
+                tmpIn = mmSocket.inputStream
+                tmpOut = mmSocket.outputStream
+            } catch (e: IOException) {
+            }
+
+            mmInStream = tmpIn
+            mmOutStream = tmpOut
+        }
+
+        override fun run() {
+            val buffer = ByteArray(1024)  // buffer store for the stream
+            var bytes: Int // bytes returned from read()
+
+            // Keep listening to the InputStream until an exception occurs
+            while (true) {
+                try {
+                    // Read from the InputStream
+                    bytes = mmInStream!!.read(buffer)
+                    print(bytes)
+                    // Send the obtained bytes to the UI activity
+                    /*mHandler.obtainMessage(MESSAGE_READ, bytes, -1, buffer)
+                        .sendToTarget()*/
+                } catch (e: IOException) {
+                    break
+                }
+
+            }
+        }
+
+        /* Call this from the main activity to send data to the remote device */
+        fun write(bytes: ByteArray) {
+            try {
+                mmOutStream!!.write(bytes)
+            } catch (e: IOException) {
+            }
+
+        }
+
+        /* Call this from the main activity to shutdown the connection */
+        fun cancel() {
+            try {
+                mmSocket.close()
+            } catch (e: IOException) {
+            }
+
+        }
+    }
+
 }
